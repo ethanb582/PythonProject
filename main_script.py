0a9557c47bbb7f08141341a9320a7f8423418fae
ethanb582 <ethanbarutha@gmail.com>
2025-03-26 09:01:29 -0500

    Update main_script.py

--- main_script.py
+++ main_script.py
@@ -1,16 +1,14 @@
-
-
 import stocks  # Import stocks.py from the same folder,
-
-checker = stocks.StockChecker() # Create an instance of StockChecker class,
 
 stock_ticker = "AAPL" # Define a stock ticker to check, # Example: Apple stock,
 
-price = checker.get_price(stock_ticker)# Call the get_price method from the class,
+checker = stocks.StockChecker(stock_ticker) # Create an instance of StockChecker class,
 
-prev_close = checker.get_previous_close(stock_ticker)# Call the get_previous_close method from the class,
+price = checker.get_price()# Call the get_price method from the class,
 
-market_cap = checker.get_market_cap(stock_ticker)# Call the get_market_cap method from the class,
+prev_close = checker.get_previous_close()# Call the get_previous_close method from the class,
+
+market_cap = checker.get_market_cap()# Call the get_market_cap method from the class,
 
 
 stocks.stock_summary(stock_ticker, price, prev_close, market_cap) # Call the stock_summary function and pass all the data,
